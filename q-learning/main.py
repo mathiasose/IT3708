00bf6ddbfb12cdbfc7998c5308bf1e92da70c15e
Mathias Ose <mathias.ose@gmail.com>
2015-05-06 16:48:20 +0200

    TD(x)

--- q-learning/main.py
+++ q-learning/main.py
@@ -17,21 +17,26 @@ if __name__ == '__main__':
     dt = INIT_TEMP / iterations
 
     replay = []
+
+    update_q = lambda q, r, b: q + agent.lr * (r + agent.dr * b - q)
+    best_q = lambda eaten, pos: max(agent.Q[eaten][pos][a] for a in agent.possible_actions)
+
     for i in xrange(iterations):
         print(i)
         while not agent.finished:
-            eat1, pos1 = agent.state
+            s1 = agent.state
             action = agent.select_action()
             reward = agent.move(action)
 
-            eat2, pos2 = agent.state
-            qs2 = agent.Q[eat2][pos2]
-            best = max(qs2[a] for a in agent.possible_actions)
+            s2 = agent.state
+            agent.memory_add((s1, action, reward, s2))
 
-            qs1 = agent.Q[eat1][pos1]
-            qs1[action] += agent.lr * (reward + agent.dr * best - qs1[action])
+            for s1, action, reward, s2 in agent.memory[::-1]:
+                e1, p1 = s1
+                e2, p2 = s2
 
-            agent.memory_add(((eat1, pos1), action, reward))
+                val = agent.Q[e1][p1][action]
+                agent.Q[e1][p1][action] = update_q(val, reward, best_q(e2, p2))
 
         if i == (iterations - 1):
             replay = agent.replay
